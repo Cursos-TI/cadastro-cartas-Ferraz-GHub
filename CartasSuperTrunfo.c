48d4a26e84457a96c50b73acb630505dd429de22
Gabriel Ferraz <gabrielferrazserafim@gmail.com>
2026-03-11 00:11:05 +0000

    Conclusão Carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,22 +10,41 @@ int main() {
   char codigo1[] = "A01";
   char cidade1[] = "Fortaleza";
 
-  int populacao1 = 2700000;
-  float area1 = 314.93;
-  float pib1 = 73.40;
-  int pontos1 = 25;
+  int populacao1 = 12325000;
+  float area1 = 1521.11;
+  float pib1 = 699.28;
+  int pontos1 = 50;
+
+  //Carta 2
+  char estado2 = 'B';
+  char codigo2[] = "B01";
+  char cidade2[] = "Curitiba";
+
+  int populacao2 = 6748000;
+  float area2 = 1200.25;
+  float pib2 = 300.50; 
+  int pontos2 = 30;
 
   // Dados da Cidade
 
-  printf("Carta 1: \n");
+  printf("Carta 1 \n");
   printf("Estado: %c \n", estado1);
   printf("Codigo: %s \n", codigo1);
   printf("Nome da Cidade: %s \n", cidade1);
   printf("Populacao: %d \n", populacao1);
   printf("Area: %2.f km2 \n", area1);
-  printf("PIB: %.2f bilhoes de reais \n", pib1);
-  printf("Numero de Pontos Turisticos: %d \n", pontos1);
+  printf("PIB: %.2f Bilhoes de Reais \n", pib1);
+  printf("Numero de Pontos Turisticos: %d \n \n", pontos1);
 
+
+  printf("Carta 2 \n");
+  printf("Estado: %c \n", estado2);
+  printf("Codigo: %s \n", codigo2);
+  printf("Nome da Cidade: %s \n", cidade2);
+  printf("Populacao: %d \n", populacao2);
+  printf("Area: %2.f Km2 \n", area2);
+  printf("PIB: %.2f Bilhoes de Reais \n", pib2);
+  printf("Numero de Pontos Turisticos: %d \n", pontos2);
 
 return 0;
 } 
